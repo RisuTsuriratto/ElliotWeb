5396030a8d05a59d935c3cafabde36c272a48b18
RisuTsuriratto <48353602+RisuTsuriratto@users.noreply.github.com>
2022-01-23 19:17:47 +0100

    Subir html, css y js y carpeta de imagenes
    Cambios hasta el 23/01/2022

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('expand-news-button').addEventListener('click', function(e) {
+        for (card of document.getElementsByClassName('card-vl')) {
+            card.style.display = 'block';
+        }
+    });
+});
